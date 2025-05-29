cad0e5ec9efd031ab7984a531c0add7ba89881b1
Leanqsd <leandro.quesada.984@gmail.com>
2025-05-29 10:13:39 -0300

    datos.page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,30 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
-  },
+  {path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+      },
+      {
+        path: 'datos',
+        loadComponent: () => import('./pages/datos/datos.page').then( m => m.DatosPage)
+      },
+    ]
+  }
+  
+
 
 ];
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  imports: [IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton],
+  imports: [RouterLink, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton],
 })
 export class MenuComponent  implements OnInit {
 
